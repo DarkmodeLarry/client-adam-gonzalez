9cf07430b2812e6ef2d5191de314b8bd53c43f98
SpiderBear <SpiderBear714@gmail.com>
2023-03-04 12:30:06 -0800

    env fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { type AppType } from 'next/app'
 import { api } from '../utils/api'
 
 import '../styles/globals.css'
-import '../styles/Calendar.css'
+import '../components/Calendar.css'
 import '../styles/Spinner.css'
 
 const MyApp: AppType = ({ Component, pageProps }) => {
